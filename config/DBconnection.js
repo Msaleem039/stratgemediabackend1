6dc09edbe81758eadc1f58c5d84da253b6cbd88a
M.saleem <Muhammad.Saleem@senew-tech.com>
2025-08-03 20:38:14 +0500

    updatedb

--- config/DBconnection.js
+++ config/DBconnection.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
-import { asyncHandler } from "../utlils/globalutils.js";
 
- export const DBconnection = asyncHandler(async()=>{
-    await mongoose.connect("mongodb+srv://saher79129:6UoV2xjvDLI13Uu3@cluster0.ydtyimm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    console.log("db connection successfully")
-})
+export const DBconnection = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://saher79129:6UoV2xjvDLI13Uu3@cluster0.ydtyimm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("DB connection successful");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1); // Stop server if DB fails
+  }
+};
